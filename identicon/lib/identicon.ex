3cd4c2c2db022ebf0c495b0a3d41d751a34e43dd
jordanhudgens <jordan@devcamp.com>
2019-03-14 22:22:20 -0500

    Updated identicon struct and called from module

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -5,8 +5,8 @@ defmodule Identicon do
     |> pick_color
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
